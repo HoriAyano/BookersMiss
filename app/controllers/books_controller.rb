71e7b063735e46a4c1f758af486837d914624761
HoriAyano <126788316+HoriAyano@users.noreply.github.com>
2023-03-16 01:24:13 +0000

    [Update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   def index
+    @list = List.new
   end
   
   def create
@@ -11,5 +12,11 @@ class BooksController < ApplicationController
   end
 
   def edit
+  end
+  
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
